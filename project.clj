c325de13b6590a951ec3d2e9ef798acf671bd84b
Donald Ball <donald.ball@livingsocial.com>
2014-11-27 11:35:48 -0500

    Abduce

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-sql "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [mysql/mysql-connector-java "5.1.34"]])
---
+++ src/clj_sql.clj
@@ -0,0 +1,60 @@
+(ns clj-sql
+  (:import [java.sql Connection]))
+
+(def isolation-levels
+  {:none             java.sql.Connection/TRANSACTION_NONE
+   :read-committed   java.sql.Connection/TRANSACTION_READ_COMMITTED
+   :read-uncommitted java.sql.Connection/TRANSACTION_READ_UNCOMMITTED
+   :repeatable-read  java.sql.Connection/TRANSACTION_REPEATABLE_READ
+   :serializable     java.sql.Connection/TRANSACTION_SERIALIZABLE})
+
+(defn isolation-level
+  [isolation]
+  (let [level (get isolation-levels isolation)]
+    (when-not level
+      (let [msg (str ("Unknown isolation level: " isolation))]
+        (throw (IllegalArgumentException. msg))))
+    level))
+
+(deftype Transaction [^Connection conn])
+
+(defn transact!
+  [^Connection conn tx-fn options]
+  (let [old-auto-commit (.getAutoCommit conn)
+        old-isolation (.getTransactionIsolation conn)
+        old-read-only (.isReadOnly conn)]
+    (io!
+     (try
+       (let [{:keys [isolation read-only]} options]
+         (.setAutoCommit conn false)
+         (when isolation
+           (.setTransactionIsolation conn (isolation-level isolation)))
+         (when read-only
+           (.setReadOnly conn true)))
+       (tx-fn (Transaction. conn))
+       (finally
+         (.setAutoCommit conn old-auto-commit)
+         (.setTransactionIsolation conn old-isolation)
+         (.setReadOnly conn old-read-only))))))
+
+(defn abduce
+  [f init tx query]
+  (let [conn (.conn tx)]
+    (when (.isClosed conn)
+      (throw (IllegalStateException. "The transaction's connection is closed")))
+    (with-open [st (.createStatement conn)
+                rs (.executeQuery st query)]
+      (let [metadata (.getMetaData rs)
+            columns (range 1 (inc (.getColumnCount metadata)))
+            keys (mapv #(keyword (.getColumnLabel metadata %)) columns)
+            next-record-fn (fn [] (zipmap keys (map #(.getObject rs %) columns)))]
+        (loop [ret init]
+          (if (.next rs)
+            (let [ret (f ret (next-record-fn))]
+              (if (reduced? ret)
+                @ret
+                (recur ret)))
+            ret))))))
+
+(def fetch
+  (partial abduce conj []))
